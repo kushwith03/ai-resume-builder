2b4073f0dd000343a742863bf731ee825615d7a5
R Khushwith Kumar <kushwith03@gmail.com>
2026-05-24 21:50:05 +0530

    Fix blank page issue caused by missing icon import in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FaCode, FaBrain, FaRocket, FaCheckCircle, FaArrowRight, FaGithub, FaTerminal } from "react-icons/fa";
+import { FaCode, FaBrain, FaRocket, FaCheckCircle, FaArrowRight, FaGithub, FaTerminal, FaExternalLinkAlt } from "react-icons/fa";
 
 function About() {
   const techStack = [
